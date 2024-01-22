21daf6065e462542fed1618f918e963aa0f2c5b9
sensei <hillarykisera@gmail.com>
2024-01-22 15:26:30 +0300

    ff

--- py_client/basics.py
+++ py_client/basics.py
@@ -1,7 +1,8 @@
 import requests
 
-endpoint = "https://httpbin.org/status/200"
-endpoint = "https://httpbin.org/"
+# endpoint = "https://httpbin.org/status/200"
+endpoint = "http://localhost:8000/"
 
-get_response = requests.get(endpoint)
-print(get_response.text)
+
+get_response = requests.get(endpoint, json={"the":"word"})
+print(get_response.status_code)
